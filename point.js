0687f73bcea7a7f24ebd6e92713e81782eae13e2
Truls Henrik Jakobsen <thj@variant.no>
2023-09-27 13:22:44 +0200

    Make distinction between physics objects and static objects

--- point.js
+++ point.js
@@ -1,59 +1,10 @@
 import { Vector } from "./vector.js";
-import { width, height } from "./script.js";
 import { ctx } from "./script.js";
 
 export class Point {
-    constructor(x, y, color = "black", vx = 0, vy = 0, drawVectors = false) {
+    constructor(x, y, color = "black") {
         this.position = new Vector(x, y);
-        this.velocity = new Vector(vx, vy);
-        this.acceleration = new Vector(0, 0);
         this.color = color;
-        this.drawVectors = drawVectors;
-        this.TOP_SPEED = 5;
-        this.STOPPING_DISTANCE = 150;
-    }
-
-    // adjust velocity to bounce off walls
-    checkCollision() {
-        if (this.position.x > width || this.position.x < 0) {
-            this.velocity.x *= -1;
-        }
-        if (this.position.y > height || this.position.y < 0) {
-            this.velocity.y *= -1;
-        }
-    }
-
-    seek(target) {
-        let desired = target.clone().sub(this.position);
-
-        let distance = desired.magnitude();
-
-        desired.normalize();
-        desired.scale(this.TOP_SPEED);
-        if (distance < this.STOPPING_DISTANCE) {
-            desired.scale((distance - 20) / this.STOPPING_DISTANCE);
-        }
-
-        desired.sub(this.velocity);
-        desired.limitForce();
-
-        if (this.drawVectors) desired.draw(this.position, "green", 1500);
-
-        this.acceleration = desired;
-    }
-
-    update() {
-        this.velocity.add(this.acceleration);
-
-        const friction = 0.01;
-        this.velocity.scale(1 - friction);
-
-        this.position.add(this.velocity);
-
-        this.oldAcceleration = this.acceleration.clone();
-        this.acceleration.zero();
-
-        this.checkCollision();
     }
 
     draw() {
@@ -61,9 +12,5 @@ export class Point {
         ctx.fillStyle = this.color;
         ctx.arc(this.position.x, this.position.y, 10, 0, 2 * Math.PI);
         ctx.fill();
-
-        if (this.drawVectors) {
-            this.velocity.draw(this.position, "red", 30);
-        }
     }
 }
--- script.js
+++ script.js
@@ -1,13 +1,18 @@
 import { Vector } from "./vector.js";
 import { Point } from "./point.js";
+import { Lizard } from "./lizard.js";
 
 function loop() {
     ctx.fillStyle = "rgba(255,255,255)";
     ctx.fillRect(0, 0, width, height);
 
-    for (let object of objects) {
-        object.update();
-        object.draw();
+    for (let pObject of physicsObjects) {
+        pObject.update();
+        pObject.draw();
+    }
+
+    for (let sObject of staticObjects) {
+        sObject.draw();
     }
 
     // have each tail segment seek the one in front of it
@@ -25,6 +30,25 @@ function loop() {
 export const canvas = document.getElementById("canvas");
 export const ctx = canvas.getContext("2d");
 
+function showObjectVectors(bool) {
+    for (let object of objects) {
+        object.drawVectors = bool;
+    }
+}
+
+// toggle show vectors if v is pressed
+document.addEventListener("keydown", function (event) {
+    if (event.key == "v") {
+        showArrows.checked = !showArrows.checked;
+        showObjectVectors(showArrows.checked);
+    }
+});
+
+const showArrows = document.getElementById("show-arrows");
+showArrows.addEventListener("change", function () {
+    showObjectVectors(showArrows.checked);
+});
+
 function getCursorPosition(canvas, event) {
     const rect = canvas.getBoundingClientRect();
     const x = event.clientX - rect.left;
@@ -33,25 +57,35 @@ function getCursorPosition(canvas, event) {
     return new Vector(x, y);
 }
 
-canvas.addEventListener("mousemove", function (event) {
-    food.position = getCursorPosition(canvas, event);
+let foodFollowsMouse = false;
+
+canvas.addEventListener("click", function (event) {
+    foodFollowsMouse = !foodFollowsMouse;
 });
 
-export const width = (canvas.width = window.innerWidth);
-export const height = (canvas.height = window.innerHeight);
+canvas.addEventListener("mousemove", function (event) {
+    if (foodFollowsMouse) food.position = getCursorPosition(canvas, event);
+});
+
+export const width = canvas.width;
+export const height = canvas.height;
 
 console.log(width, height);
 
-const bob = new Point(100, 100, "red", 1, 1, true);
-const food = new Point(width / 2, height / 2, "green");
-
 const tailLength = 10;
+const bob = new Lizard(10 + 20 * tailLength, 10 + 20 * tailLength, "red");
+
 let tail = [];
 
 for (let i = 0; i < tailLength; i++) {
-    tail.push(new Point(0, 0, "blue", 0, 0, true));
+    tail.push(new Lizard(10 + i * 20, 10 + i * 20, "blue"));
 }
 
-const objects = [bob, food, ...tail];
+tail.reverse();
+
+const food = new Point(width / 2, height / 2, "green");
+
+const physicsObjects = [bob, ...tail];
+const staticObjects = [food];
 
 loop();
